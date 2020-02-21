fa1d239f5a01e32e87499a16e738739c2ad67024
Demo User <debian@beaglebone.localdomain>
2020-02-21 13:04:01 +0000

    Threading

--- demux.py
+++ demux.py
@@ -1,4 +1,8 @@
 from bbb_pru_adc.capture import capture 
+import threading
+
+measure=threading.Lock()
+
 t=[]
 
 with capture(clk_div=893,step_avg=0,channels=[0],auto_install=False,max_num=1,target_delay=9999999) as cap:
