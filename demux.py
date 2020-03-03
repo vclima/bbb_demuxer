6c2a13afe7deb785f5735b7c444c1c685ad403af
Demo User <debian@beaglebone.localdomain>
2020-03-03 14:41:06 +0000

    IQ functions

--- demux.py
+++ demux.py
@@ -5,21 +5,22 @@ import numpy as np
 
 measure_lock=threading.Lock()
 
-def ADC_read():
+def ADC_read(m,n):
 
    global measure
    global read_delay
    global new_read
    global kill_thread
 
-   Fs=20
-   T=2
+   F_sig=4
+   Fs=F_sig*n/m
+   
 
    measure=[0,0]
 
    clk=int(17000/Fs)
    clk_delay=int((1/(Fs*5e-9))-1)
-   samples=int(Fs*T)
+   samples=n
 
    with capture(clk_div=clk,step_avg=0,channels=[0, 1],auto_install=False,max_num=samples,target_delay=clk_delay) as cap:
       for num_dropped, timestamps, values in cap:
@@ -37,6 +38,15 @@ def ADC_read():
    
    print('Exiting ADC thread')
 
+def IQ(m,n,adc_raw):
+   j=np.arange(0,n)
+   sin_vec=np.sin(j*2*np.pi*m/n)
+   cos_vec=np.cos(j*2*np.pi*m/n)
+
+   I=(2/n)*np.sum(np.multiply(cos_vec,adc_raw))
+   Q=(2/n)*np.sum(np.multiply(sin_vec,adc_raw))
+
+   return [I,Q]
 
 def main():
 
@@ -45,29 +55,35 @@ def main():
    global new_read
    global kill_thread
    global adc0
-   global adc1
+   global MO
    global sample_time
 
    kill_thread=0
    new_read=0
 
-   ADC_thread=threading.Thread(target=ADC_read)
+   n=15
+   m=4
+   
+
+   ADC_thread=threading.Thread(target=ADC_read,args=(m,n))
    ADC_thread.start()
    adc0=[]
-   adc1=[]
+   MO=[]
    sample_time=[]
    samples=0
    try:
       while(1):
          if(new_read):
             adc0.append(measure[0])
-            adc1.append(measure[1])
+            MO.append(measure[1])
             sample_time.extend(read_delay)
             measure_lock.acquire()
             new_read=0
             measure_lock.release()
             samples+=1
+            [I_MO,Q_MO]=IQ(m,n,MO)
             print('Samples: ',samples)
+            print('I_MO:',I_MO,'Q_MO:',Q_MO)
    except KeyboardInterrupt:
       kill_thread=1
       print('Sending kill ADC thread signal')
@@ -80,12 +96,7 @@ def main():
 
    print('Average sample time:',avg_sample)
    print('Average Jitter:',jitter)
-   '''
-   t=np.linspace(0,len(sample_time)*5e-9,len(sample_time))
-   plt.plot(t,adc0,'r',t,adc1,'b')
-   plt.title('ADC Readings')
-   plt.show()
-   '''
+
 
    
 
