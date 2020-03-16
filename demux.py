bcabd3d3ea8de6ef2af1d614cec08ab0d6bd15b2
Demo User <debian@beaglebone.localdomain>
2020-03-16 16:47:52 +0000

    IQ measurement

--- demux.py
+++ demux.py
@@ -1,6 +1,7 @@
 from bbb_pru_adc.capture import capture 
 import threading
 import numpy as np
+import pickle
 
 
 measure_lock=threading.Lock()
@@ -12,7 +13,7 @@ def ADC_read(m,n):
    global new_read
    global kill_thread
 
-   F_sig=4
+   F_sig=40
    Fs=F_sig*n/m
    
 
@@ -48,6 +49,19 @@ def IQ(m,n,adc_raw):
 
    return [I,Q]
 
+def IQ2(quad,adc_raw):
+   sin_vec=1
+   cos_vec=1
+
+   if(quad):
+      I=-cos_vec*adc_raw[0]
+      Q=-sin_vec*adc_raw[1]
+
+   if(not(quad)):
+      I=cos_vec*adc_raw[0]
+      Q=sin_vec*adc_raw[1]
+   return [I,Q]
+
 def amp(I,Q):
    amp=np.sqrt(I**2+Q**2)
    return amp
@@ -68,12 +82,13 @@ def main():
    global adc0
    global MO
    global sample_time
-
+   
    kill_thread=0
    new_read=0
 
-   n=15
+   n=16
    m=4
+   #quad=0
    
 
    ADC_thread=threading.Thread(target=ADC_read,args=(m,n))
@@ -81,27 +96,48 @@ def main():
    adc0=[]
    MO=[]
    sample_time=[]
+
+   MO_save=[]
+   meas_save=[]
+   amp_save=[]
+   phs_save=[]
+   adc0_save=[]
+   adc1_save=[]
+
    samples=0
    try:
       while(1):
          if(new_read):
-            adc0=measure[0]
-            MO=measure[1]
+            adc0=np.array(measure[0])
+            MO=np.array(measure[1])
+            adc0_save.append(adc0)
+            adc1_save.append(MO)
             sample_time.extend(read_delay)
             measure_lock.acquire()
             new_read=0
             measure_lock.release()
             samples+=1
             [I_MO,Q_MO]=IQ(m,n,MO)
+            #[I_MO,Q_MO]=IQ2(quad,MO)
+            MO_save.append([I_MO,Q_MO])
             [I,Q]=IQ(m,n,adc0)
+            #[I,Q]=IQ2(quad,adc0)
+            meas_save.append([I,Q])
+            amp_disp=amp(I,Q)
+            amp_save.append(amp_disp)
+            phs_disp=phase(I,Q,I_MO,Q_MO)
+            phs_save.append(phs_disp)
             print('Samples: ',samples)
             print('I_MO:',I_MO,'Q_MO:',Q_MO)
             print('I:',I,'Q:',Q)
-            print('AMP:',amp(I,Q),'Phase',phase(I,Q,I_MO,Q_MO))
+            print('AMP:',amp_disp,'Phase',phs_disp)
+            #quad=not(quad)
    except KeyboardInterrupt:
       kill_thread=1
       print('Sending kill ADC thread signal')
       ADC_thread.join()
+      with open('measurements.pickle','wb') as f:
+         pickle.dump([MO_save,meas_save,amp_save,phs_save,adc0_save,adc1_save],f)
 
    print('ADC status is ',ADC_thread.isAlive())
 
