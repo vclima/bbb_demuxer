9138850fd42848e0ccd83e00b19b46fbd78bc4c5
Victor Lima <victor.lima.314@gmail.com>
2020-03-18 12:29:06 -0300

    renamed pickle files

--- plot.py
+++ plot.py
@@ -1,24 +1,36 @@
 import pickle
 import matplotlib.pyplot as plt 
-import numpy as np 
+import numpy as np
 
-with open('e.pickle','rb') as f:
+with open('a.pickle','rb') as f:
     vars_f=pickle.load(f)
 
-t=np.linspace(10,200,num=4000)
+#t=np.linspace(10,200,num=4000)
 
 spec=np.array(vars_f[2])
-ind=np.where(spec<0.05)[0][0]
-spec=spec[ind:ind+4000]
-plt.plot(t,spec)
+#ind=np.where(spec<0.05)[0][0]
+#spec=spec[ind:ind+4000]
+plt.plot(spec)
 
-with open('d.pickle','rb') as f:
+with open('b.pickle','rb') as f:
     vars_f=pickle.load(f)
 
-t=np.linspace(10,200,num=4000)
+#t=np.linspace(10,200,num=4000)
 
 spec=np.array(vars_f[2])
-ind=np.where(spec<0.05)[0][0]
-spec=spec[ind:ind+4000]
-plt.plot(t,spec)
+#ind=np.where(spec<0.05)[0][0]
+#spec=spec[ind:ind+4000]
+plt.plot(spec)
+
+
+
+with open('c.pickle','rb') as f:
+    vars_f=pickle.load(f)
+
+#t=np.linspace(10,200,num=4000)
+
+spec=np.array(vars_f[2])
+#ind=np.where(spec<0.05)[0][0]
+#spec=spec[ind:ind+4000]
+plt.plot(spec)
 plt.show()
