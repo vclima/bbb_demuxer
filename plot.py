07d738c422347fd32390859bb54032c0f51f8b77
Victor Lima <victor.lima.314@gmail.com>
2020-03-27 14:40:08 -0300

    3/1

--- plot.py
+++ plot.py
@@ -38,70 +38,72 @@ def phase(I,Q,I_MO,Q_MO):
 
    return deg
 
-with open('4_1.pickle','rb') as f:
+with open('3_1.pickle','rb') as f:
     vars_f=pickle.load(f)
 
 adc_raw=np.array(vars_f[4])
-[I_adc,Q_adc]=IQ(adc_raw)
+#[I_adc,Q_adc]=IQ(adc_raw)
 
 mo_raw=np.array(vars_f[5])
-[I_mo,Q_mo]=IQ(mo_raw)
+#[I_mo,Q_mo]=IQ(mo_raw)
 
-amp_IQ2=amp(I_adc,Q_adc)
-ph_IQ2=phase(I_adc,Q_adc,I_mo,Q_mo)
+#amp_IQ2=amp(I_adc,Q_adc)
+#ph_IQ2=phase(I_adc,Q_adc,I_mo,Q_mo)
 
-with open('4_1.pickle','rb') as f:
+with open('3_1.pickle','rb') as f:
     vars_f=pickle.load(f)
 
 amp_IQ4=np.array(vars_f[2])
 ph_IQ4=np.array(vars_f[3])
 
-with open('8_2.pickle','rb') as f:
+with open('7_2.pickle','rb') as f:
     vars_f=pickle.load(f)
 
 amp_IQ8=np.array(vars_f[2])
 ph_IQ8=np.array(vars_f[3])
 
-with open('16_4.pickle','rb') as f:
+with open('15_4.pickle','rb') as f:
     vars_f=pickle.load(f)
 
 amp_IQ16=np.array(vars_f[2])
 ph_IQ16=np.array(vars_f[3])
 
-max_len=np.min([len(amp_IQ2),len(amp_IQ4),len(amp_IQ8),len(amp_IQ16)])
+#max_len=np.min([len(amp_IQ2),len(amp_IQ4),len(amp_IQ8),len(amp_IQ16)])
+max_len=np.min([len(amp_IQ4),len(amp_IQ8),len(amp_IQ16)])
 
 ph_IQ4[(ph_IQ4<0)]=ph_IQ4[(ph_IQ4<0)]+360
 
-amp_IQ2=amp_IQ2[0:max_len]
+#amp_IQ2=amp_IQ2[0:max_len]
 amp_IQ4=amp_IQ4[0:max_len]
 amp_IQ8=amp_IQ8[0:max_len]
 amp_IQ16=amp_IQ16[0:max_len]
 
-ph_IQ2=ph_IQ2[0:max_len]
+#ph_IQ2=ph_IQ2[0:max_len]
 ph_IQ4=ph_IQ4[0:max_len]
 ph_IQ8=ph_IQ8[0:max_len]
 ph_IQ16=ph_IQ16[0:max_len]
 
 
 plt.subplot(211)
-plt.plot(amp_IQ2,label='IQ 2 samples')
-plt.plot(amp_IQ4,label='IQ 4 samples')
-plt.plot(amp_IQ8,label='IQ 8 samples')
-plt.plot(amp_IQ16,label='IQ 16 samples')
+#plt.plot(amp_IQ2,label='IQ 2 samples')
+plt.plot(amp_IQ4,label='IQ 3 samples')
+plt.plot(amp_IQ8,label='IQ 7 samples')
+plt.plot(amp_IQ16,label='IQ 15 samples')
 plt.legend()
 
 plt.subplot(212)
-plt.plot(ph_IQ2)
+#plt.plot(ph_IQ2)
 plt.plot(ph_IQ4)
 plt.plot(ph_IQ8)
 plt.plot(ph_IQ16)
 
 
-SNR2=20*np.log(np.mean(amp_IQ2)/np.std(amp_IQ2))
+#SNR2=20*np.log(np.mean(amp_IQ2)/np.std(amp_IQ2))
 SNR4=20*np.log(np.mean(amp_IQ4)/np.std(amp_IQ4))
 SNR8=20*np.log(np.mean(amp_IQ8)/np.std(amp_IQ8))
 SNR16=20*np.log(np.mean(amp_IQ16)/np.std(amp_IQ16))
 
-print(SNR2,SNR4,SNR8,SNR16)
+#print(SNR2,SNR4,SNR8,SNR16)
+print(SNR4,SNR8,SNR16)
 
 plt.show()
